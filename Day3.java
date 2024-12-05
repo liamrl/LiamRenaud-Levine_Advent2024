74ddbf39f18ae8ba186dad917e58bd9751a1391b
liamrl <146093586+liamrl@users.noreply.github.com>
2024-12-05 14:06:27 -0500

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Day3 {
+    public static void main(String[] args) {
+
+        ArrayList<String> fileData = getFileData("src/Day3Input.txt");
+
+        Pattern pattern = Pattern.compile("")
+
+    }
+
+
+
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+
+
+}
