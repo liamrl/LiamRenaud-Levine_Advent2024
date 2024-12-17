2508cf20d634ccf60c3361f1cf5d161e0d0da355
liamrl <146093586+liamrl@users.noreply.github.com>
2024-12-17 14:04:11 -0500

    Add files via upload

--- Day6.java
+++ Day6.java
@@ -2,23 +2,107 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Day6 {
     public static void main(String[] args) {
 
-        ArrayList<String> fileData = getFileData("src/Day3Input.txt");
+        ArrayList<String> fileData = getFileData("src/Day6Input.txt");
 
         int row = fileData.size();
         int column = fileData.get(0).length();
 
-        String[][] map = new String[row][col];
-        
+        String[][] map = new String[row][column];
+
+        int posX = 0;
+        int posY = 0;
+
+        final int LASTY = fileData.size() - 1;
+        final int LASTX = fileData.get(0).length() - 1;
+
         for (int i = 0; i < fileData.size(); i++){
             for (int j = 0; j < fileData.get(0).length(); j++){
-                map[i][j] = fileData.get(i).indexOf(j);
+                if (fileData.get(i).substring(j, j + 1).equals("^")){
+                    posX = j;
+                    posY = i;
+                }
+                map[i][j] = fileData.get(i).substring(j, j + 1);
+            }
+        }
+
+
+
+        boolean up = true;
+        boolean right = false;
+        boolean down = false;
+        boolean left = false;
+
+
+
+        while (true){
+            if (up){
+                if (posY == 0){
+                    map[posY][posX] = "X";
+                    break;
+                } else if (map[posY - 1][posX].equals("#")){
+                    up = false;
+                    right = true;
+                } else{
+                    map[posY][posX] = "X";
+                    posY--;
+                }
+
+            }
+
+            if (right){
+                if (posX == LASTX){
+                    map[posY][posX] = "X";
+                    break;
+                } else if (map[posY][posX + 1].equals("#")){
+                    right = false;
+                    down = true;
+                } else{
+                    map[posY][posX] = "X";
+                    posX++;
+                }
+            }
+
+            if (down){
+                if (posY == LASTY){
+                    map[posY][posX] = "X";
+                    break;
+                } else if (map[posY + 1][posX].equals("#")){
+                    down = false;
+                    left = true;
+                } else{
+                    map[posY][posX] = "X";
+                    posY++;
+                }
+
+            }
+
+            if (left){
+                if (posX == 0){
+                    map[posY][posX] = "X";
+                    break;
+                } else if (map[posY][posX - 1].equals("#")){
+                    left = false;
+                    up = true;
+                } else{
+                    map[posY][posX] = "X";
+                    posX--;
+                }
+            }
+
+        }
+
+        int total = 0;
+
+        for (String[] str : map){
+            for (String s : str){
+                if (s.equals("X")){
+                    total++;
+                }
             }
         }
 
@@ -28,6 +112,11 @@ public class Day6 {
             }
             System.out.println();
         }
+
+        System.out.println(total);
+
+
+
 
     }
 
